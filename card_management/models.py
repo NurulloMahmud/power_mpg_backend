ae60381f3ab0737ec90e3d6d1643cd540d3ebd63
NurullohMahmud <nurullomahmud@gmail.com>
2024-06-05 16:12:47 -0500

    sd

--- card_management/models.py
+++ card_management/models.py
@@ -13,26 +13,11 @@ class Card(models.Model):
     def __str__(self):
         return self.card_number
 
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            if not self.company:
-                company, created = "users.Company.objects.get_or_create(name='PowerMPG', owner='PowerMPG', phone_number='', email='', price_category=1, is_active=True)"
-            else:
-                company = self.company
-            CardDriverHistory.objects.create(card=self, driver=self.driver, beg_date=self.created_at, company=company)
-        else:
-            card_driver = CardDriverHistory.objects.get(card=self, end_date__isnull=True)
-            if self.driver != card_driver.driver:
-                card_driver.end_date = datetime.now()
-                card_driver.save()
-                CardDriverHistory.objects.create(card=self, driver=self.driver, beg_date=datetime.now(), company=self.company)
-        super().save(*args, **kwargs)
-
 class CardDriverHistory(models.Model):
-    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    card = models.CharField(max_length=25, null=True, blank=True)
     driver = models.CharField(max_length=100, null=True, blank=True)
-    company = models.ForeignKey('users.Company', on_delete=models.CASCADE, null=True, blank=True)
-    beg_date = models.DateField()
+    company = models.CharField(max_length=100, null=True, blank=True)
+    beg_date = models.DateField(auto_now_add=True)
     end_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
--- card_management/serializers.py
+++ card_management/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Card, CardDriverHistory
+from datetime import datetime
 
 
 class CardReadSerializer(serializers.ModelSerializer):
@@ -19,6 +20,36 @@ class CardWriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = "__all__"
+
+    def create(self, validated_data):
+        card = Card.objects.create(**validated_data)
+        if card.driver is not None:
+            CardDriverHistory.objects.create(
+                card=card.card_number,
+                driver=card.driver,
+                company=card.company,
+            )
+        return card
+
+    def update(self, instance, validated_data):
+        # check if the driver is changing
+        driver = validated_data.get("driver")
+        if driver is not None and driver != instance.driver:
+            old_card_history = CardDriverHistory.objects.filter(
+                card=instance.card_number,
+                end_date__isnull=True,
+            ).first()
+            if old_card_history is not None:
+                old_card_history.end_date = datetime.now()
+                old_card_history.save()
+                
+            # create new history
+            CardDriverHistory.objects.create(
+                card=instance.card_number,
+                driver=driver,
+                company=instance.company,
+            )
+        return super().update(instance, validated_data)
 
 class CardDriverHistorySerializer(serializers.ModelSerializer):
     company = serializers.SerializerMethodField()
