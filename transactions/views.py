f69720a31a89ebd5caa11628ac9a83d34eba2d71
NurulloMahmud <nurullomahmud@gmail.com>
2024-06-24 16:00:24 +0500

    files

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
+from django.core.files.storage import FileSystemStorage
+from django.db import transaction
+from .models import Transaction
+from users.permissions import IsAdminRole
+import pandas as pd
+import os
 
-# Create your views here.
+
+
+class TransactionCreateView(APIView):
+    permission_classes = [IsAuthenticated, IsAdminRole]
+
+    def post(self, request):
+        try:
+            file = request.FILES['file']
+        except:
+            context = {
+                "success": False,
+                "message": "Missing file",
+            }
+            return Response(context, status=status.HTTP_400_BAD_REQUEST)
+        
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+        file_path = fs.path(filename)
+
+        # Read the Excel file and specify the dtype for store_id
+        df = pd.read_excel(file_path, dtype={'store_id': str})
+        df = df.fillna('')
+
+        # Populate the database
--- users/models.py
+++ users/models.py
@@ -8,6 +8,8 @@ class Company(models.Model):
     phone_number = models.CharField(max_length=14, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     price_category = models.PositiveIntegerField(null=True, blank=True) # remove null later
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    mailing_address = models.CharField(max_length=100, null=True, blank=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
