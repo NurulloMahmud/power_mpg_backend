0351dabd7d3f42edd393e44c6c008aafb2cbb99b
NurullohMahmud <nurullomahmud@gmail.com>
2024-07-01 11:23:04 -0500

    db

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Company, CustomUser
+from .models import Company, CustomUser, CompanyStatus
 
 
 admin.site.register(Company)
 admin.site.register(CustomUser)
+admin.site.register(CompanyStatus)
