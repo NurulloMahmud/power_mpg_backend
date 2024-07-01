d41b5afec7b453ebb143ab672bc9328635d97f97
NurullohMahmud <nurullomahmud@gmail.com>
2024-07-01 12:43:35 -0500

    account crud

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 from .views import AccountTypeViewSet
 from rest_framework.routers import DefaultRouter
 from .views import (
-    PaymentListCreateView
+    PaymentListCreateView, AccountViewSet
 )
 
 
 router = DefaultRouter()
 router.register(r'accounttype', AccountTypeViewSet, basename='accounttype')
+router.register(r'account', AccountViewSet, basename='account')
 
 urlpatterns = [
     path('', include(router.urls)),
